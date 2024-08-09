547de301b238529ad3b1fdf52fc2a0295d9b7c09
ErNishantNeupane <npn.nishant94@gmail.com>
2024-08-09 11:28:55 -0400

    Changes in requirements.txt and app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config(page_title="Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("Chat with PDF using LLAMA 3💁")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
